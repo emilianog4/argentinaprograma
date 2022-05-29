8dd94e0622f4e4c809dfb9c9605638e642ea2c0a
emilianog4 <emiliano.gutierrez4@gmail.com>
2022-05-29 18:58:09 -0300

    probando

--- src/main/java/com/ap/argentinaprograma/ArgentinaprogramaApplication.java
+++ src/main/java/com/ap/argentinaprograma/ArgentinaprogramaApplication.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
-
+//
+//@CrossOrigin(origins = "https://portfoliofront-e2bb3.web.app")
 @SpringBootApplication
 public class ArgentinaprogramaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArgentinaprogramaApplication.class, args);
+                
 	}
         
         @Bean
