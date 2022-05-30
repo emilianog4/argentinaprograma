0556a5435cfd81dca84ef751260226a0504bff0c
emilianog4 <emiliano.gutierrez4@gmail.com>
2022-05-30 19:52:09 -0300

    correccion de errores

--- src/main/java/com/ap/argentinaprograma/security/MainSecurity.java
+++ src/main/java/com/ap/argentinaprograma/security/MainSecurity.java
@@ -58,7 +58,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
